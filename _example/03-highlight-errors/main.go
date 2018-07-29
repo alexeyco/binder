d5cdaab5bf428280c586c7e037d661b2ea2dbe08
Alexey Popov <6425762@gmail.com>
2018-07-29 18:16:14 +0300

    Beautify errors

---
+++ _example/03-highlight-errors/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"errors"
+	"log"
+
+	"github.com/alexeyco/binder"
+				"os"
+	)
+
+type Person struct {
+	Name string
+}
+
+func main() {
+	b := binder.New()
+
+	t := b.Table("person")
+	t.Static("new", func(c *binder.Context) error {
+		if c.Top() == 0 {
+			return errors.New("need arguments")
+		}
+		n := c.Arg(1).String()
+
+		c.Push().Data(&Person{n}, "person")
+		return nil
+	})
+
+	t.Dynamic("name", func(c *binder.Context) error {
+		p, ok := c.Arg(1).Data().(*Person)
+		if !ok {
+			return errors.New("person expected")
+		}
+
+		if c.Top() == 1 {
+			c.Push().String(p.Name)
+		} else {
+			p.Name = c.Arg(2).String()
+		}
+
+		return nil
+	})
+
+	if err := b.DoString(`
+local p = person.new('Steeve')
+print(p:name())
+
+p:name('Alice')
+print(p:name())
+
+-- person table does not have method "email"
+print(p:email())
+	`); err != nil {
+		switch err.(type) {
+		case *binder.Error:
+			e := err.(*binder.Error)
+			e.Print()
+
+			os.Exit(0)
+			break
+		default:
+			log.Fatalln(err)
+		}
+	}
+}
---
+++ error.go
@@ -0,0 +1,166 @@
+package binder
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/yuin/gopher-lua"
+)
+
+const (
+	// errorLinesBefore lines before problem line
+	errorLinesBefore = 5
+
+	// errorLinesBefore lines after problem line
+	errorLinesAfter = 5
+)
+
+type errSourceHandler func(problem int) *source
+
+type line struct {
+	number  int
+	problem bool
+	code    string
+}
+
+func (l *line) String(numbers int) string {
+	return fmt.Sprintf("%s %s", l.numberString(numbers), l.code)
+}
+
+func (l *line) numberString(pad int) string {
+	output := fmt.Sprintf("%d", l.number)
+	pad -= len(output)
+
+	if pad <= 0 {
+		return output
+	}
+
+	return fmt.Sprintf("%s%s", strings.Repeat(" ", pad), output)
+}
+
+type source struct {
+	lines []*line
+}
+
+func (s *source) problem() string {
+	output := make([]string, len(s.lines))
+	numbers := s.numbersMaxLength()
+
+	for i, l := range s.lines {
+		output[i] = l.String(numbers)
+	}
+
+	return strings.Join(output, "\n")
+}
+
+func (s *source) numbersMaxLength() int {
+	l := 0
+	for _, line := range s.lines {
+		length := len(fmt.Sprintf("%d", line.number))
+		if length > l {
+			l = length
+		}
+	}
+
+	return l
+}
+
+func newSource(code string, problem int) *source {
+	l := strings.Split(code, "\n")
+	count := len(l)
+	lines := make([]*line, count)
+
+	for i, v := range l {
+		lines[i] = &line{
+			number:  i + 1,
+			problem: i+1 == problem,
+			code:    v,
+		}
+	}
+
+	length := errorLinesBefore + errorLinesAfter + 1
+
+	if length <= count {
+		before := problem - errorLinesBefore
+		after := problem + errorLinesAfter
+
+		if before < 0 {
+			before = 0
+			after = before + length
+		}
+
+		if after > count-1 {
+			after = count - 1
+			before = after - length
+		}
+
+		lines = lines[before:after]
+	}
+
+	return &source{
+		lines: lines,
+	}
+}
+
+// Error error object
+type Error struct {
+	error   string
+	problem int
+	source  *source
+}
+
+// Error returns error string
+func (e *Error) Error() string {
+	if e.problem >= 0 {
+		return fmt.Sprintf("Line %d: %s", e.problem, e.error)
+	}
+
+	return e.error
+}
+
+// Source returns problem source code as string
+func (e *Error) Source() string {
+	return e.source.problem()
+}
+
+// Print prints problem source code
+func (e *Error) Print() {
+	fmt.Println(e.Error())
+	fmt.Println()
+	fmt.Println(e.Source())
+}
+
+func newError(err error, h errSourceHandler) error {
+	switch err.(type) {
+	case *lua.ApiError:
+		e := err.(*lua.ApiError)
+		p := strings.Split(e.Object.String(), ":")
+		c := len(p)
+
+		var (
+			problem = -1
+			text    = e.Error()
+		)
+
+		if c > 1 {
+			if v, err := strconv.Atoi(p[1]); err == nil {
+				problem = v
+			}
+		}
+
+		if c > 2 {
+			text = strings.Trim(p[2], " ")
+		}
+
+		return &Error{
+			error:   text,
+			problem: problem,
+			source:  h(problem),
+		}
+
+		break
+	}
+
+	return err
+}
